5fb2ac6e86e29b872e68e179698093dd0bb79943
Filip Sobol <sobol.filip@gmail.com>
2020-04-06 19:05:38 +0200

    Fix tests for Framework package

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+    preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/packages/$1",
+        "^@framework/(.*)$": "<rootDir>/packages/framework/src/$1",
+    }
 };
